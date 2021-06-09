b76dfff3faf65aa6a774b2d3df288dea31ef003a
nandit123 <nanditmehra123@gmail.com>
2021-06-10 05:10:50 +0530

    changed key size

--- u2f-core/src/constants.rs
+++ u2f-core/src/constants.rs
@@ -17,7 +17,7 @@ pub(crate) const AUTH_ENFORCE: u8 = 0x03; // Enforce user presence and sign
 pub(crate) const AUTH_CHECK_ONLY: u8 = 0x07; // Check only
 pub(crate) const AUTH_DONT_ENFORCE: u8 = 0x08; // Don't enforce user presence and sign
 
-pub(crate) const DEFAULT_KEY_HANDLE_LEN: usize = 255;
-pub(crate) const MAX_KEY_HANDLE_LEN: usize = 255;
+pub(crate) const DEFAULT_KEY_HANDLE_LEN: usize = 200;
+pub(crate) const MAX_KEY_HANDLE_LEN: usize = 200;
 
 pub(crate) const EC_POINT_FORMAT_UNCOMPRESSED: u8 = 0x04;
